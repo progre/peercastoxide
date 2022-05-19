75a5c4576120da1a4c70650eb4b5c9b324aa35df
progre <progre+pgp@prgrssv.net>
2022-05-19 21:33:59 +0900

    fix(pcpproxy_gui): line height

--- pcpproxy_gui/src/components/ConnectionsView.tsx
+++ pcpproxy_gui/src/components/ConnectionsView.tsx
@@ -6,7 +6,7 @@ import {
   VariableSizeNodeData,
 } from 'react-vtree';
 import { NodeData } from 'react-vtree/dist/es/Tree';
-import { Atom } from '../App';
+import { Atom, AtomChild, AtomParent } from '../App';
 import TreesView from './TreesView';
 
 export interface Connection {
@@ -32,20 +32,119 @@ function Identifier(props: { identifier: string }): JSX.Element {
   );
 }
 
+function AtomParentView(props: {
+  data: AtomParent;
+  isOpen: boolean;
+  toggle(): void;
+}): JSX.Element {
+  return (
+    <DefaultButton
+      onClick={() => {
+        props.toggle();
+      }}
+      css={css`
+        border: none;
+        margin-top: 1px;
+        height: 29px;
+
+        > span > * {
+          display: flex;
+          align-items: center;
+          height: 16px;
+        }
+      `}
+    >
+      <Icon
+        iconName={props.isOpen ? 'chevrondown' : 'chevronrightmed'}
+        css={css`
+          width: 16px;
+        `}
+      />
+      <div
+        css={css`
+          margin-left: 8px;
+          width: 32px;
+        `}
+      >
+        <Identifier identifier={props.data.identifier} />
+      </div>
+    </DefaultButton>
+  );
+}
+
+function AtomChildView(props: { data: AtomChild }): JSX.Element {
+  return (
+    <div
+      css={css`
+        height: 32px;
+        margin-left: 40px;
+        display: flex;
+
+        > div {
+          display: flex;
+          align-items: center;
+        }
+      `}
+    >
+      <div>
+        <Identifier identifier={props.data.identifier} />
+      </div>
+      <div
+        css={css`
+          margin-left: 1em;
+          white-space: pre;
+          ${typeof props.data.payload !== 'string' ||
+          props.data.payload.split('\n').length <= 0
+            ? null
+            : 'font-size: 13px; line-height: 16px;'}
+        `}
+      >
+        {typeof props.data.payload !== 'string'
+          ? props.data.payload?.join?.(', ')
+          : props.data.payload.replaceAll('\r', '␍').replaceAll('\n', '␊\n')}
+      </div>
+    </div>
+  );
+}
+
+function Raw(props: { payload: string }) {
+  return (
+    <div
+      css={css`
+        height: 32px;
+        margin-left: 40px;
+        display: flex;
+        white-space: pre;
+
+        display: flex;
+        align-items: center;
+        ${props.payload.split('\n').length <= 0
+          ? null
+          : 'font-size: 13px; line-height: 16px;'}
+      `}
+    >
+      {props.payload.replaceAll('\r', '␍').replaceAll('\n', '␊\n')}
+    </div>
+  );
+}
+
 function Field(
   props: VariableSizeNodeComponentProps<
     Atom & NodeData & VariableSizeNodeData & { nestingLevel: number }
   >
 ): JSX.Element {
   useEffect(() => {
-    if (!('children' in props.data) && typeof props.data.payload === 'string') {
-      const lines = props.data.payload.split('\n').length;
-      if (lines > 1) {
-        props.resize(lines * 16, true);
-      } else {
-        props.resize(32, true);
-      }
+    console.log(props.data.identifier);
+    if ('children' in props.data || typeof props.data.payload !== 'string') {
+      props.resize(32, true);
+      return;
     }
+    const lines = props.data.payload.split('\n').length;
+    if (lines <= 1) {
+      props.resize(32, true);
+      return;
+    }
+    props.resize(lines * 16, true);
   }, [props.resize, props.data, props.height]);
   return (
     <div
@@ -60,70 +159,15 @@ function Field(
       `}
     >
       {'children' in props.data ? (
-        <DefaultButton
-          onClick={() => {
-            props.toggle();
-          }}
-          css={css`
-            border: none;
-            margin-top: 1px;
-            height: 29px;
-
-            > span > * {
-              display: flex;
-              align-items: center;
-              height: 16px;
-            }
-          `}
-        >
-          <Icon
-            iconName={props.isOpen ? 'chevrondown' : 'chevronrightmed'}
-            css={css`
-              width: 16px;
-            `}
-          />
-          <div
-            css={css`
-              margin-left: 8px;
-              width: 32px;
-            `}
-          >
-            <Identifier identifier={props.data.identifier} />
-          </div>
-        </DefaultButton>
+        <AtomParentView
+          data={props.data as AtomParent}
+          isOpen={props.isOpen}
+          toggle={props.toggle}
+        />
+      ) : props.data.identifier === '#RAW' ? (
+        <Raw payload={props.data.payload as string} />
       ) : (
-        <div
-          css={css`
-            height: 32px;
-            margin-left: 40px;
-            display: flex;
-
-            > div {
-              display: flex;
-              align-items: center;
-            }
-          `}
-        >
-          <div>
-            <Identifier identifier={props.data.identifier} />
-          </div>
-          <div
-            css={css`
-              margin-left: 1em;
-              white-space: pre;
-              ${typeof props.data.payload !== 'string' ||
-              props.data.payload.split('\n').length <= 0
-                ? null
-                : 'font-size: 13px; line-height: 16px;'}
-            `}
-          >
-            {typeof props.data.payload !== 'string'
-              ? props.data.payload?.join?.(', ')
-              : props.data.payload
-                  .replaceAll('\r', '␍')
-                  .replaceAll('\n', '␊\n')}
-          </div>
-        </div>
+        <AtomChildView data={props.data} />
       )}
     </div>
   );
